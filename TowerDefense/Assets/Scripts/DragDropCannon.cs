c6d2f437f2a7d4f97388f93ce19ee2c18e3e6504
DESKTOP-M72LUD9\arsalansul <arsalansul@gmail.com>
2017-08-09 04:11:24 +0700

    дроп тавера, но не везде(

--- TowerDefense/Assets/Scripts/DragDropCannon.cs
+++ TowerDefense/Assets/Scripts/DragDropCannon.cs
@@ -2,72 +2,56 @@
 using System.Collections;
 using System.Linq;
 using Assets.Scripts;
-using GameClient.UI;
-
+using UnityEngine.EventSystems;
 /// <summary>
 /// Drag and drop mechanism
 /// </summary>
-public class DragDropCannon : MonoBehaviour
-{
-
+public class DragDropCannon : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{        
     // Use this for initialization
     void Start()
     {
         mainCamera = Camera.main;
+        newCannon = Instantiate(CannonPrefab);
+        newCannon.SetActive(false);
     }
    
     private Camera mainCamera;
-    //type of bunnies we'll create
     public GameObject CannonPrefab;
-    //the starting object for the drag
-    public GameObject CannonGenerator;
-    bool isDragging = false;
-    //temp Cannon
     private GameObject newCannon;
 
     //will be colored red if we cannot place a Cannon there
     private GameObject tempBackgroundBehindPath;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //if we have money and we can drag a new Cannon
-        
-        if (Input.GetMouseButtonDown(0) && !isDragging && GameManager.Instance.MoneyAvailable >= Constants.CannonCost)
-        {
-            ResetTempBackgroundColor();
-            Vector2 location = mainCamera.ScreenToWorldPoint(Input.mousePosition);            
-            //if user has tapped onto the Cannon generator
-            if (CannonGenerator.GetComponent<CircleCollider2D>() ==Physics2D.OverlapPoint(location, 1 << LayerMask.NameToLayer("CannonGenerator")))
-            {
-                //initiate dragging operation and create a new Cannon for us to drag
-                isDragging = true;
-                //create a temp Cannon to drag around
-                newCannon = Instantiate(CannonPrefab, CannonGenerator.transform.position, Quaternion.identity) as GameObject;
-            }
-        }
-        else if (Input.GetMouseButton(0) && isDragging)
-        {
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit[] hits = Physics.RaycastAll(ray.origin, ray.direction);
-            if (hits.Length > 0 && hits[0].collider != null)
-            {
-                newCannon.transform.position = hits[0].collider.gameObject.transform.position;
+    private GameObject selected = null;
 
-                //if we're hitting a path or tower
-                //or there is an existing Cannon there
-                //we use > 1 since we're hovering over the newCannon gameobject 
-                //(i.e. there is already a Cannon there)
-                if (hits.Where(x=>x.collider.gameObject.tag == "Tower").Count() > 0
-                    || hits.Where(x=>x.collider.gameObject.tag == "Cannon").Count() > 1)
+    public void OnBeginDrag(PointerEventData data)
+    {
+        selected = data.pointerPressRaycast.gameObject;
+    }    
+
+    public void OnDrag(PointerEventData data)
+    {
+        if (selected.gameObject.tag == "CannonGenerator" && GameManager.Instance.MoneyAvailable >= Constants.CannonCost)
+        {                  
+            RaycastHit[] hits;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            hits = Physics.RaycastAll(ray, 50f);
+
+            if (hits.Length > 0 && hits != null)
+            {
+                newCannon.transform.position = hits[0].transform.position;
+                newCannon.SetActive(true);
+                
+                if (hits.Where(x => x.collider.gameObject.tag == "Tower").Count() > 0
+                    || hits.Where(x => x.collider.gameObject.tag == "Cannon").Count() > 1)
                 {
                     //we cannot place a Cannon there
-                    GameObject backgroundBehindPath = hits.Where
-                        (x => x.collider.gameObject.tag == "Background").First().collider.gameObject;
+                    GameObject backgroundBehindPath = hits.Where(x => x.collider.gameObject.tag == "Background").First().collider.gameObject;
                     //make the sprite material "more red"
                     //to let the user know that we can't place a Cannon here
-                    backgroundBehindPath.GetComponent<SpriteRenderer>().color = Constants.RedColor;
-                    
+                    backgroundBehindPath.GetComponent<MeshRenderer>().material.color = Constants.RedColor;
+
                     if (tempBackgroundBehindPath != backgroundBehindPath)
                         ResetTempBackgroundColor();
                     //cache it to revert later
@@ -79,42 +63,63 @@ public class DragDropCannon : MonoBehaviour
                 }
 
             }
-        }
-        //we're stopping dragging
-        else if (Input.GetMouseButtonUp(0) && isDragging)
-        {
-            ResetTempBackgroundColor();
-            //check if we can leave the Cannon here
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-
-            RaycastHit[] hits = Physics.RaycastAll(ray.origin, ray.direction, Mathf.Infinity, ~(1 << LayerMask.NameToLayer("CannonGenerator")));
-            //in order to place it, we must have a background and no other bunnies
-            if (hits.Where(x=>x.collider.gameObject.tag == "Background").Count() > 0
-                && hits.Where(x => x.collider.gameObject.tag == "Path").Count() == 0
-                && hits.Where(x=>x.collider.gameObject.tag == "Cannon").Count() == 1)
-            {
-                //we can leave a Cannon here, so decrease money and activate it
-                GameManager.Instance.AlterMoneyAvailable(-Constants.CannonCost);
-                newCannon.transform.position = 
-                    hits.Where(x => x.collider.gameObject.tag == "Background")
-                    .First().collider.gameObject.transform.position;
-                newCannon.GetComponent<Cannon>().Activate();
-            }
             else
-            {
-                //we can't leave a Cannon here, so destroy the temp one
-                Destroy(newCannon);
-            }
-            isDragging = false;
+                newCannon.SetActive(false);
 
         }
+
     }
 
+    public void OnEndDrag(PointerEventData data)
+    {
+        selected = null;
+        ResetTempBackgroundColor();
+
+        // If the prefab instance is active after dragging stopped, it means
+        // it's in the arena so (for now), just drop it in.
+        if (newCannon.activeSelf)
+        {
+            Instantiate(CannonPrefab, newCannon.transform.position, Quaternion.identity);
+        }
+
+        // Then set it to inactive ready for the next drag!
+        newCannon.SetActive(false);
+
+        ////in order to place it, we must have a background and no other bunnies
+        //if (hits.Where(x => x.collider.gameObject.tag == "Background").Count() > 0
+        //    && hits.Where(x => x.collider.gameObject.tag == "Path").Count() == 0
+        //    && hits.Where(x => x.collider.gameObject.tag == "Cannon").Count() == 1)
+        //{
+        //    //we can leave a Cannon here, so decrease money and activate it
+        //    GameManager.Instance.AlterMoneyAvailable(-Constants.CannonCost);
+        //    newCannon.transform.position = hits.Where(x => x.collider.gameObject.tag == "Background").First().collider.gameObject.transform.position;
+        //    newCannon.GetComponent<Cannon>().Activate();
+        //}
+        //else
+        //{
+        //    //we can't leave a Cannon here, so destroy the temp one
+        //    Destroy(newCannon);
+        //}
+
+    }
+    //popov end
+    int GetTerrainColliderQuadIndex(RaycastHit[] hits)
+    {
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i].collider.gameObject.name.Equals("TerrainColliderQuad"))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
     //make background sprite appear as it is
     private void ResetTempBackgroundColor()
     {
         if (tempBackgroundBehindPath != null)
-            tempBackgroundBehindPath.GetComponent<SpriteRenderer>().color = Constants.BlackColor;
+            tempBackgroundBehindPath.GetComponent<MeshRenderer>().material.color = Constants.BlackColor;
     }
-
+    
 }
