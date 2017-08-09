50e292f1c6acfe941a8763164c2a4ce2f9586e07
DESKTOP-M72LUD9\arsalansul <arsalansul@gmail.com>
2017-08-09 18:04:08 +0700

    подсветка бэкграунда

--- TowerDefense/Assets/Scripts/Constants.cs
+++ TowerDefense/Assets/Scripts/Constants.cs
@@ -13,6 +13,7 @@ namespace Assets.Scripts
     {
         public static readonly Color RedColor = new Color(1f, 0f, 0f, 0f);
         public static readonly Color BlackColor = new Color(0f, 0f, 0f, 0f);
+        public static readonly Color BackgroundColor = new Color(132/255f, 147/255f, 48/255f, 0f);
         public static readonly int CannonCost = 50;
         public static readonly int CarrotAward = 10;
         public static readonly int InitialEnemyHealth = 50;
--- TowerDefense/Assets/Scripts/DragDropCannon.cs
+++ TowerDefense/Assets/Scripts/DragDropCannon.cs
@@ -18,12 +18,7 @@ public class DragDropCannon : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
    
     private Camera mainCamera;
     public GameObject CannonPrefab;
-    private GameObject newCannon;
-    void Update()
-    {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(ray.origin, ray.direction * 50, Color.yellow);
-    }
+    private GameObject newCannon;   
 
     //will be colored red if we cannot place a Cannon there
     private GameObject tempBackgroundBehindPath;
@@ -41,7 +36,7 @@ public class DragDropCannon : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
         {                  
             RaycastHit[] hits;
             Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            hits = Physics.RaycastAll(ray, 500f);
+            hits = Physics.RaycastAll(ray, 50f);
 
             if (hits.Length > 0 && hits != null)
             {
@@ -50,66 +45,49 @@ public class DragDropCannon : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
                 {
                     newCannon.transform.position = hits[terrainCollderQuadIndex].point;
                     newCannon.SetActive(true);
+                    if (hits.Where(x => x.collider.gameObject.tag == "Tower").Count() > 0 || hits.Where(x => x.collider.gameObject.tag == "Cannon").Count() > 1)
+                    {
+                        //we cannot place a Cannon there
+                        GameObject backgroundBehindPath = hits.Where(x => x.collider.gameObject.tag == "Background").First().collider.gameObject;
+                        //make the sprite material "more red"
+                        //to let the user know that we can't place a Cannon here
+                        backgroundBehindPath.GetComponent<MeshRenderer>().material.color = Constants.RedColor;
+
+                        if (tempBackgroundBehindPath != backgroundBehindPath)
+                            ResetTempBackgroundColor();
+                        //cache it to revert later
+                        tempBackgroundBehindPath = backgroundBehindPath;
+
+                        newCannon.SetActive(false);
+                    }
+                    else //just reset the color on previously set paths
+                    {
+                        ResetTempBackgroundColor();
+                    }
                 }
-                
-                //if (hits.Where(x => x.collider.gameObject.tag == "Tower").Count() > 0
-                //    || hits.Where(x => x.collider.gameObject.tag == "Cannon").Count() > 1)
-                //{
-                //    //we cannot place a Cannon there
-                //    GameObject backgroundBehindPath = hits.Where(x => x.collider.gameObject.tag == "Background").First().collider.gameObject;
-                //    //make the sprite material "more red"
-                //    //to let the user know that we can't place a Cannon here
-                //    backgroundBehindPath.GetComponent<MeshRenderer>().material.color = Constants.RedColor;
-                //
-                //    if (tempBackgroundBehindPath != backgroundBehindPath)
-                //        ResetTempBackgroundColor();
-                //    //cache it to revert later
-                //    tempBackgroundBehindPath = backgroundBehindPath;
-                //}
-                //else //just reset the color on previously set paths
-                //{
-                //    ResetTempBackgroundColor();
-                //}
                 else
-                    newCannon.SetActive(false);
-            }
-           
+                    newCannon.SetActive(false);                
+            }         
 
         }
-
     }
 
     public void OnEndDrag(PointerEventData data)
     {
         selected = null;
-        //ResetTempBackgroundColor();
+        
+        ResetTempBackgroundColor();
 
         // If the prefab instance is active after dragging stopped, it means
         // it's in the arena so (for now), just drop it in.
         if (newCannon.activeSelf)
         {
             Instantiate(CannonPrefab, newCannon.transform.position, Quaternion.identity);
-        }
+            GameManager.Instance.AlterMoneyAvailable(-Constants.CannonCost);
+        }       
 
         // Then set it to inactive ready for the next drag!
-        newCannon.SetActive(false);
-
-        ////in order to place it, we must have a background and no other bunnies
-        //if (hits.Where(x => x.collider.gameObject.tag == "Background").Count() > 0
-        //    && hits.Where(x => x.collider.gameObject.tag == "Path").Count() == 0
-        //    && hits.Where(x => x.collider.gameObject.tag == "Cannon").Count() == 1)
-        //{
-        //    //we can leave a Cannon here, so decrease money and activate it
-        //    GameManager.Instance.AlterMoneyAvailable(-Constants.CannonCost);
-        //    newCannon.transform.position = hits.Where(x => x.collider.gameObject.tag == "Background").First().collider.gameObject.transform.position;
-        //    newCannon.GetComponent<Cannon>().Activate();
-        //}
-        //else
-        //{
-        //    //we can't leave a Cannon here, so destroy the temp one
-        //    Destroy(newCannon);
-        //}
-
+        newCannon.SetActive(false);       
     }
     //popov end
     int GetTerrainColliderQuadIndex(RaycastHit[] hits)
@@ -128,7 +106,7 @@ public class DragDropCannon : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
     private void ResetTempBackgroundColor()
     {
         if (tempBackgroundBehindPath != null)
-            tempBackgroundBehindPath.GetComponent<MeshRenderer>().material.color = Constants.BlackColor;
+            tempBackgroundBehindPath.GetComponent<MeshRenderer>().material.color=Constants.BackgroundColor;
     }
     
 }
